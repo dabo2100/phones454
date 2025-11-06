d37b69bc8f906a4d4e9ac862f7eba5246579629b
Dabo-2100 <a_fattah_m@icloud.com>
2025-11-06 22:06:01 +0200

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
 import { useRef, useState } from 'react';
+import Swal from 'sweetalert2';
 
 export default function App() {
-  // ref
   const nameInput = useRef();
   const priceInput = useRef();
   const qtyInput = useRef();
-
+  const [editPhoneIndex, setEditPhoneIndex] = useState();
   const [newPhoneModalIndex, setNewPhoneModalIndex] = useState(false);
-
-  const [phones, setPhones] = useState([
-    { name: 'iPhone x', price: 400, qty: 3 },
-    { name: 'iPhone 11', price: 500, qty: 4 },
-    { name: 'iPhone 12', price: 600, qty: 5 },
-    { name: 'iPhone 13', price: 700, qty: 2 },
-  ]);
-
-  const removePhone = (index) => {
-    let isConfirm = confirm('Are you sure ?');
-    if (isConfirm) {
-      let copy = [...phones];
-      copy.splice(index, 1);
-      setPhones(copy);
-    }
-  };
+  const [phones, setPhones] = useState(JSON.parse(localStorage.getItem('phones')) || []);
 
   const addNewPhone = () => {
     let newPhone = {
@@ -30,12 +15,61 @@ export default function App() {
       price: +priceInput.current.value,
       qty: +qtyInput.current.value,
     };
-
     let copy = [...phones];
     copy.push(newPhone);
     setPhones(copy);
-    alert('Phone New Added');
+    Swal.fire({
+      icon: 'success',
+      text: 'Phone Added Succsussfuly !',
+    });
     setNewPhoneModalIndex(false);
+    localStorage.setItem('phones', JSON.stringify(copy));
+  };
+
+  const removePhone = (index) => {
+    Swal.fire({
+      icon: 'question',
+      text: 'Are you sure you want to delete ',
+      showConfirmButton: true,
+      showCancelButton: true,
+      confirmButtonColor: 'green',
+      confirmButtonText: 'نعم امسح ',
+      cancelButtonText: 'لا متمتسحش حاجة ',
+    }).then((res) => {
+      if (res.isConfirmed) {
+        let copy = [...phones];
+        copy.splice(index, 1);
+        setPhones(copy);
+        localStorage.setItem('phones', JSON.stringify(copy));
+        Swal.fire({
+          icon: 'success',
+          text: 'Phone Deleted Successfully !',
+        });
+      }
+    });
+  };
+
+  const openEditModal = (index) => {
+    setEditPhoneIndex(index);
+    document.getElementById('my_modal_1').showModal();
+    let oldData = phones[index];
+    nameInput.current.value = oldData.name;
+    priceInput.current.value = oldData.price;
+    qtyInput.current.value = oldData.qty;
+  };
+
+  const saveNewData = () => {
+    let newPhoneData = {
+      name: nameInput.current.value,
+      price: +priceInput.current.value,
+      qty: +qtyInput.current.value,
+    };
+    let copy = [...phones];
+    copy[editPhoneIndex] = newPhoneData;
+    setPhones(copy);
+    alert('Phone Data Edit Success');
+    console.log('I Edit Phone Index : ' + editPhoneIndex);
+    localStorage.setItem('phones', JSON.stringify(copy));
   };
 
   return (
@@ -68,7 +102,9 @@ export default function App() {
                   <td>{el.price * el.qty}</td>
                   <td>
                     <div className="w-full flex justify-center gap-3">
-                      <button className="btn btn-warning">Edit</button>
+                      <button className="btn btn-warning" onClick={() => openEditModal(index)}>
+                        Edit
+                      </button>
                       <button className="btn btn-error" onClick={() => removePhone(index)}>
                         Remove
                       </button>
@@ -101,28 +137,21 @@ export default function App() {
         </div>
       )}
 
-      {/* <button className="btn" onClick={() => document.getElementById('my_modal_1').showModal()}>
-        open modal
-      </button>
-      <dialog id="my_modal_1" className="modal">
-        <div className="modal-box">
+      <dialog id="my_modal_1" className="modal" onClick={() => document.querySelector('#my_modal_1').close()}>
+        <div className="modal-box" onClick={(event) => event.stopPropagation()}>
           <h3 className="font-bold text-lg">Edit Phone Data</h3>
           <div className="modal-action">
             <form method="dialog" className="flex flex-col gap-4 w-full">
               <input ref={nameInput} type="text" className="w-full input" placeholder="Enter new phone Name" />
               <input ref={priceInput} type="text" className="w-full input" placeholder="Enter new phone Price" />
               <input ref={qtyInput} type="text" className="w-full input" placeholder="Enter new phone Qty" />
-              <button className="btn">Save</button>
+              <button className="btn" onClick={saveNewData}>
+                Save
+              </button>
             </form>
           </div>
         </div>
-      </dialog> */}
+      </dialog>
     </div>
   );
 }
-
-// Read
-// Delete
-// props - state managet
-// Add
-// Update
